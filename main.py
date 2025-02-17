26c7dc7f983a039a9f64b0fbe3d4e9c15bfd50e9
anuragsinghbhandari <anuoo3ups@gmail.com>
2025-02-17 20:24:48 +0530

    Improved functioning of buttons

--- main.py
+++ main.py
@@ -11,37 +11,50 @@ from models.llama_model_1 import little_llama_answer
 from models.middle_llama import middle_llama_answer
 from models.event_summarizer import summarize
 
-if 'ds_clicked' not in st.session_state:
-    st.session_state.ds_clicked = False
-if 'ps_clicked' not in st.session_state:
-    st.session_state.ps_clicked = False
-if 'plan_disable' not in st.session_state:
-    st.session_state.plan_disable = True
+for key in ['ds_clicked', 'ps_clicked', 'plan_disable', 'plan']:
+    if key not in st.session_state:
+        st.session_state[key] = True if key in ['plan_disable'] else False
+for var in ['topic', 'name1', 'prof1','name2', 'prof2','plan','Discussion']:
+    if var not in st.session_state:
+        st.session_state[var] = ""
 
-Discussion = ""
-plan = ""
-topic = st.text_input("Topic for discussion")
-name1 = st.text_input("Name of 1st Character",)
-prof1 = st.text_input(f"Profession of {name1}",key='name1')
-name2 = st.text_input("Name of second Character")
-prof2 = st.text_input(f"Profession of {name2}",key='name2')
+form = st.form("Input form")
+st.session_state.topic = form.text_input("Topic for discussion")
+st.session_state.name1 = form.text_input("Name of 1st Character",)
+st.session_state.prof1 = form.text_input(f"Profession of 1st Character")
+st.session_state.name2 = form.text_input("Name of 2nd Character")
+st.session_state.prof2 = form.text_input(f"Profession of 2nd Character")
 
+states = [st.session_state[var] for var in ['topic', 'name1', 'name2', 'prof1', 'prof2']]
 def discuss_button():
-    st.session_state.ds_clicked = True
+    flag=0
+    for i in range(len(states)):
+        if states[i] == '':
+            st.warning(f"Please fill {i} field before starting.")
+            flag=1
+            break   
+    if flag ==0:
+        st.session_state.ds_clicked = True
 
 def plan_button():
-    st.session_state.ps_clicked = True    
-start_clicked = st.button("Start",on_click=discuss_button)
+    st.session_state.ps_clicked = True   
+    st.session_state.plan_disable = True
+start_clicked = form.form_submit_button("Start",on_click=discuss_button)
+
 if st.session_state.ds_clicked:
-    st.title(f"{topic} Discussion")
+    st.title(f"{st.session_state.topic} Discussion")
     for i in range(10):
-        l_answer,Discussion = little_llama_answer(name1,prof1,topic,Discussion)
-        st.write(f"{name1}: {l_answer}")
-        m_answer,Discussion = middle_llama_answer(name2,prof2,topic,Discussion)
-        st.write(f"{name2}: {m_answer}")
-    plan = summarize(Discussion)
+        l_answer,st.session_state.Discussion = little_llama_answer(st.session_state.name1,st.session_state.prof1,st.session_state.topic,st.session_state.Discussion)
+        st.write(f"{st.session_state.name1}: {l_answer}")
+        m_answer,st.session_state.Discussion = middle_llama_answer(st.session_state.name2,st.session_state.prof2,st.session_state.topic,st.session_state.Discussion)
+        st.write(f"{st.session_state.name2}: {m_answer}")
+    st.session_state.plan = summarize(st.session_state.Discussion)
     st.session_state.plan_disable= False
+    st.session_state.ds_clicked = False
+
 plan_clicked = st.button("Summarize",on_click=plan_button, disabled=st.session_state.plan_disable)
 
 if st.session_state.ps_clicked:
-    st.markdown(f"concluder: {plan}")
+    
+    st.markdown(f"concluder: {st.session_state.plan}")
+    
